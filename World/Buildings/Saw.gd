224ec35a9f24339571250d99c3e2519375e96b39
Gongure <62403880+Gongure@users.noreply.github.com>
2021-08-15 22:05:30 +0200

    Gute Nacht

--- World/Buildings/Saw.gd
+++ World/Buildings/Saw.gd
@@ -4,5 +4,5 @@ func interaction_can_interact(interactionComponentParent : Node) -> bool:
 	return interactionComponentParent is Lumberjack
 
 func interaction_interact(interactionComponentParent : Node) -> void:
-	pass
-	
+	var wood_check_notifier = interactionComponentParent.get_parent()
+	wood_check_notifier.check_wood()
